18b0ea2905e1c35ef39f144ae1fce7bc2ba5ed3e
zyzlaozhang <Laozhang2011@163.com>
2022-05-24 17:29:18 +0800

    pynotfile

---
+++ python/pyfile.py
@@ -0,0 +1,9 @@
+from pynotifier import Notification
+
+Notification(
+	title='Notification Title',
+	description='Notification Description',
+	 
+	duration=5,                                   # Duration in seconds
+	
+).send()
