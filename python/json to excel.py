cde31e9622a16c438d7ad6b9523d0adae189b3f7
zyzlaozhang <Laozhang2011@163.com>
2022-05-25 10:55:49 +0800

    mongodb to excel

---
+++ python/json to excel.py
@@ -0,0 +1,30 @@
+from re import L
+import pandas as pd
+import pymongo
+import xlwt
+ke=[]
+va=[]
+j=0
+l=0
+i=1
+book = xlwt.Workbook(encoding='utf-8',style_compression=0)
+sheet = book.add_sheet('json',cell_overwrite_ok=True)
+client = pymongo.MongoClient(host= '127.0.0.1', port=27017)
+db = client.testdb
+coll = db.get_collection("namedb")
+find_post = coll.find_one({'_id' : '10'})
+for key,value in find_post.items():
+    ke.clear()
+    ke.append(key)
+    ke.append(value)
+    print(ke)
+    i=1
+    l=l+1
+    for i in range(len(ke)):
+        
+        print(ke[i])
+        
+        sheet.write(l,i,ke[i])
+    
+savepath = 'D:\\excel表格2.xls'
+book.save(savepath)
